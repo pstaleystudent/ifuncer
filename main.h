d8313ac409cf15fc3be9ce9310a96d3ce26d94e5
pstaley <pstaleyoff@gmail.com>
2022-11-29 17:33:33 -0500

    initial commit - just the plan

---
+++ main.h
@@ -0,0 +1,24 @@
+#pragma once
+class main {
+	/*
+	todo:
+	capture arguments
+	compile base and SIMD objects
+	rename base and SIMD object functions
+	generate ifunc resolver
+	compile resolver object
+	merge base, SIMD, resolver objects with ld
+	cleanup
+	provide template makefile
+	documentation
+
+	extensions:
+	output location switch
+	failed vectorization notification
+	arbitrary arguement switches
+	remove failed vectorization
+	toggle archetecture switch
+	arbitrary optimization switches
+	*/
+};
+
